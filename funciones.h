29ee3873a486a7d08c92d064769967371dcc0162
LeodzARG <132969407+LeodzARG@users.noreply.github.com>
2023-10-30 00:31:17 -0300

    funciones.h

---
+++ funciones.h
@@ -0,0 +1,15 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <string>
+
+// Declaración de la función
+void ingresarNombres(std::string &nombre1, std::string &nombre2);
+bool confirmarNombres(std::string player1, std::string player2);
+#endif
+
+/*def FUNCIONES_H
+#define FUNCIONES_H
+
+#endif*/ /*a esto se le llama guards esta para que el archivo no sea incluido mas de una vez en un archivo cpp.
+*/
