4633ffb8446900c2ba1af7afdeb2a453ff032d06
LeodzARG <132969407+LeodzARG@users.noreply.github.com>
2023-10-30 16:39:22 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -6,6 +6,7 @@
 // Declaración de la función
 void ingresarNombres(std::string &nombre1, std::string &nombre2);
 bool confirmarNombres(std::string player1, std::string player2);
+int tirardados (int limite);
 #endif
 
 /*def FUNCIONES_H
