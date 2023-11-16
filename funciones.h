df3b2d9de61f0f8b23451725c3c6248786b1bfd1
Santi <147670790+SantiRR96@users.noreply.github.com>
2023-11-16 14:21:31 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -8,6 +8,8 @@ void ingresarNombres(std::string &nombre1, std::string &nombre2);
 bool confirmarNombres(std::string player1, std::string player2);
 void informar_carta(std::string &valor, std::string &palo);
 void barajar_cartas(std::string valores[], std::string palos[], int tamano);
+int obtener_indice_carta(const std::string &valor);
+bool cartas_en_orden(const std::string valores[]);
 //int tirardados (int limite);
 
 #endif
