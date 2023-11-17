96e18af18786166a290d2dee86b083ca4e848bde
LeodzARG <132969407+LeodzARG@users.noreply.github.com>
2023-11-16 22:31:53 -0300

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -41,15 +41,15 @@ bool confirmarNombres(string player1, string player2) {
 	return (confirm == 'S' || confirm == 's');
 	
 }
-	
-	int tirardados (int limite){
-		
-		return (rand() % limite)+1;
-	}
 
-/// Aca hago el mazo de cartas y reparte 
+int tirardados (int tam){
 	
-void informar_carta(string &valor, string &palo) {
+	return (rand() % tam)+1;
+}
+	
+	/// Aca hago el mazo de cartas y reparte 
+	
+	void informar_carta(string &valor, char &palo) {
 		const int total_cartas = 20;
 		static int cartas_restantes = total_cartas;
 		static bool numeros_generados[total_cartas] = {false};
@@ -68,86 +68,92 @@ void informar_carta(string &valor, string &palo) {
 		numeros_generados[carta] = true;
 		cartas_restantes--;
 		
+		char TREBOL = 5;
+		char CORAZON = 3;
+		char DIAMANTE = 4;
+		char PICA = 6;
 		switch (carta) {
+			
+			
 		case 0:
 			valor = "10";
-			palo = "PICA";
+			palo = PICA;
 			break;
 		case 1:
 			valor = "J";
-			palo = "PICA";
+			palo = PICA;
 			break;
 		case 2:
 			valor = "K";
-			palo = "PICA";
+			palo = PICA;
 			break;
 		case 3:
 			valor = "Q";
-			palo = "PICA";
+			palo = PICA;
 			break;
 		case 4:
 			valor = "A";
-			palo = "PICA";
+			palo = PICA;
 			break;
 		case 5:
 			valor = "10";
-			palo = "CORAZONES";
+			palo = CORAZON;
 			break;
 		case 6:
 			valor = "J";
-			palo = "CORAZONES";
+			palo = CORAZON;
 			break;
 		case 7:
 			valor = "K";
-			palo = "CORAZONES";
+			palo = CORAZON;
 			break;
 		case 8:
 			valor = "Q";
-			palo = "CORAZONES";
+			palo = CORAZON;
 			break;
 		case 9:
 			valor = "A";
-			palo = "CORAZONES";
+			palo = CORAZON;
 			break;
 		case 10:
 			valor = "10";
-			palo = "TREBOL";
+			palo = TREBOL;
 			break;
 		case 11:
 			valor = "J";
-			palo = "TREBOL";
+			palo = TREBOL;
 			break;
 		case 12:
 			valor = "K";
-			palo = "TREBOL";
+			palo = TREBOL;
 			break;
 		case 13:
 			valor = "Q";
-			palo = "TREBOL";
+			palo = TREBOL;
 			break;
 		case 14:
 			valor = "A";
-			palo = "TREBOL";
+			palo = TREBOL;
 			break;
 		case 15:
 			valor = "10";
-			palo = "DIAMANTE";
+			palo = DIAMANTE;
 			break;
 		case 16:
 			valor = "J";
-			palo = "DIAMANTE";
+			palo = DIAMANTE;
 			break;
 		case 17:
 			valor = "K";
-			palo = "DIAMANTE";
+			palo = DIAMANTE;
 			break;
 		case 18:
 			valor = "Q";
-			palo = "DIAMANTE";
+			palo = DIAMANTE;
 			break;
 		case 19:
 			valor = "A";
-			palo = "DIAMANTE";
+			palo = DIAMANTE;
 			break;
 		default:;
 		}
@@ -163,155 +169,53 @@ void informar_carta(string &valor, string &palo) {
 			swap(palos[i], palos[j]);
 		}
 	}
-
-int obtener_indice_carta(const std::string &valor) {
-	if (valor == "10") {
-		return 10;
-	} else if (valor == "J") {
-		return 11;
-	} else if (valor == "Q") {
-		return 12;
-	} else if (valor == "K") {
-		return 13;
-	} else {
-		return 14;
-	}
-}
-
-
-bool cartas_en_orden(const string valores[]) {
-	const int CARTAS_POR_JUGADOR = 5;
-	for (int i = 0; i < CARTAS_POR_JUGADOR - 1; ++i) {
-		if (valores[i] > valores[i + 1]) {
-			return false; // Las cartas no están en orden
-		}
-	}
-	return true; // Las cartas están en orden
-}	
-
-int contar_cartas(const string valores[], const string &carta) {
-	int contador = 0;
-	const int CARTAS_POR_JUGADOR = 5;
-	for (int i = 0; i < CARTAS_POR_JUGADOR; ++i) {
-		if (valores[i] == carta) {
-			contador++;
-		}
-	}
-	return contador;
-}
-
-int comparar_cartas(const string valores_jugador1[], const string valores_jugador2[], const string &carta) {
-	int cantidad_jugador1 = contar_cartas(valores_jugador1, carta);
-	int cantidad_jugador2 = contar_cartas(valores_jugador2, carta);
 	
-	if (cantidad_jugador1 > cantidad_jugador2) {
-		return 1; // Jugador 1 tiene más cartas de la carta especificada
-	} else if (cantidad_jugador1 < cantidad_jugador2) {
-		return -1; // Jugador 2 tiene más cartas de la carta especificada
-	} else {
-		return 0; // Ambos jugadores tienen la misma cantidad de cartas de la carta especificada
+	int obtener_indice_carta(const std::string &valor) {
+		if (valor == "10") {
+			return 10;
+		} else if (valor == "J") {
+			return 11;
+		} else if (valor == "Q") {
+			return 12;
+		} else if (valor == "K") {
+			return 13;
+		} else {
+			return 14;
+		}
 	}
-}
-
-void dados_accion(int resultado_dado, std::string valores_propios[], std::string valores_contrario[], std::string mazo[]){
-	switch (resultado_dado) {
-	case 1:
-		// Elegir una carta propia y robar del mazo
-		// Intercambiar las dos cartas
-		// La carta robada se incorpora al corral propio y la seleccionada al mazo
-	{
-		int carta_propia;
-		std::cout << "Elige una carta de tu propio corral (1-5): ";
-		std::cin >> carta_propia;
-		
-		// Intercambiar las cartas
-		std::swap(valores_propios[carta_propia - 1], mazo[0]);
-	}
-	break;
-	case 2:
-		// Elegir una carta del corral contrario y robar del mazo
-		// Intercambiar las dos cartas
-		// La carta robada se incorpora al corral contrario y la seleccionada al mazo
-	{
-		int carta_contrario;
-		std::cout << "Elige una carta del corral contrario (1-5): ";
-		std::cin >> carta_contrario;
 	
-		// Intercambiar las cartas
-		std::swap(valores_contrario[carta_contrario - 1], mazo[0]);
-	}
-	break;
-	case 3:
-		// Elegir una carta propia e intercambiarla con una del corral contrario
-	{
-		int carta_propia, carta_contrario;
-		std::cout << "Elige una carta de tu propio corral (1-5): ";
-		std::cin >> carta_propia;
-		
-		// Validar la entrada del usuario
-		if (carta_propia < 1 || carta_propia > 5) {
-			std::cout << "Entrada inválida. Debes elegir un número entre 1 y 5." << std::endl;
-			return;
+	
+	bool cartas_en_orden(const string valores[]) {
+		const int CARTAS_POR_JUGADOR = 5;
+		for (int i = 0; i < CARTAS_POR_JUGADOR - 1; ++i) {
+			if (valores[i] > valores[i + 1]) {
+				return false; // Las cartas no están en orden
+			}
 		}
-		
-		std::cout << "Elige una carta del corral contrario (1-5): ";
-		std::cin >> carta_contrario;
-		
-		// Validar la entrada del usuario
-		if (carta_contrario < 1 || carta_contrario > 5) {
-			std::cout << "Entrada inválida. Debes elegir un número entre 1 y 5." << std::endl;
-			return;
+		return true; // Las cartas están en orden
+	}	
+	
+	int contar_cartas(const string valores[], const string &carta) {
+		int contador = 0;
+		const int CARTAS_POR_JUGADOR = 5;
+		for (int i = 0; i < CARTAS_POR_JUGADOR; ++i) {
+			if (valores[i] == carta) {
+				contador++;
+			}
 		}
-		
-		// Intercambiar las cartas
-		std::swap(valores_propios[carta_propia - 1], valores_contrario[carta_contrario - 1]);
+		return contador;
 	}
-	break;
-	case 4:
-		// Intercambiar dos cartas propias
-		break;
-	case 5:
-		std::cout << "Acción 5: Bloquear una carta del corral.\n";
-		// Lógica para la acción 5
-		// ...
-		break;
+	
+	int comparar_cartas(const string valores_jugador1[], const string valores_jugador2[], const string &carta) {
+		int cantidad_jugador1 = contar_cartas(valores_jugador1, carta);
+		int cantidad_jugador2 = contar_cartas(valores_jugador2, carta);
 		
-	case 6:
-		int opcion;
-		std::cout << "Acción 6: Elegir cualquiera de las acciones anteriores o bien pasar el turno.\n";
-		std::cout << "Selecciona la acción (1-6, 0 para pasar el turno): ";
-		std::cin >> opcion;
-		
-		switch (opcion) {
-		case 1:
-			// Lógica para la acción 1
-			// ...
-			break;
-		case 2:
-			// Lógica para la acción 2
-			// ...
-			break;
-		case 3:
-			// Lógica para la acción 3
-			// ...
-			break;
-		case 4:
-			// Lógica para la acción 4
-			// ...
-			break;
-		case 5:
-			// Lógica para la acción 5
-			// ...
-			break;
-		default:
-			std::cout << "Turno pasado.\n";
-			break;
+		if (cantidad_jugador1 > cantidad_jugador2) {
+			return 1; // Jugador 1 tiene más cartas de la carta especificada
+		} else if (cantidad_jugador1 < cantidad_jugador2) {
+			return -1; // Jugador 2 tiene más cartas de la carta especificada
+		} else {
+			return 0; // Ambos jugadores tienen la misma cantidad de cartas de la carta especificada
 		}
-		
-		break;
-		
-	default:
-		std::cout << "Resultado del dado inválido.\n";
-		break;
 	}
-}
+
