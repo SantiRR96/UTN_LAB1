1e7756437e8bfc28991b2fcad916a10a892eeca3
LeodzARG <132969407+LeodzARG@users.noreply.github.com>
2023-11-16 22:32:16 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -5,17 +5,25 @@
 
 // Declaración de la función
 void ingresarNombres(std::string &nombre1, std::string &nombre2);
+
 bool confirmarNombres(std::string player1, std::string player2);
-void informar_carta(std::string &valor, std::string &palo);
+
+void informar_carta(std::string &valor,  char &palo);
+
 void barajar_cartas(std::string valores[], std::string palos[], int tamano);
+
 int obtener_indice_carta(const std::string &valor);
+
 bool cartas_en_orden(const std::string valores[]);
+
 int contar_cartas(const std::string valores[], const std::string &carta);
+
 int comparar_cartas(const std::string valores_jugador1[], const std::string valores_jugador2[], const std::string &carta);
-void dados_accion(int resultado_dado, std::string valores_propios[], std::string valores_contrario[], std::string mazo[]);
-//int tirardados (int limite);
+
+int tirardados (int tam); 
+
+
 
 #endif
 
-/*def FUNCIONES_H
-#define FUNCIONES_H
+
